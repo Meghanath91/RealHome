c9ded69451a97bb7826fcb6d18f15dcd6e0b9590
Meghanath91 <meghanath.balaji@gmail.com>
2020-05-31 16:28:16 -0400

    route add

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home/Home";
+import Properties from "./components/Properties/Properties";
+import Review from "./components/Reviews/Reviews";
+import Contact from "./components/Contact/Contact";
+import News from "./components/News/News";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+
+          <Route path="/properties">
+            <Properties />
+          </Route>
+
+          <Route path="/review">
+            <Review />
+          </Route>
+
+          <Route path="/contact">
+            <Contact />
+          </Route>
+
+          <Route path="/News">
+            <News />
+          </Route>
+          
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
